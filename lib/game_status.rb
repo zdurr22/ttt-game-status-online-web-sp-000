218df41cb1f1edbe66f8104437586ec44de17138
Zachary Durr <zdurr22@gmail.com>
2020-09-24 16:28:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,11 +24,12 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if position_1 == "X" && position_2 =="X" && position_3 = "X" || if position_1 == "O" && position_2 =="O" && position_3 = "O"
+    if position_1 == "X" && position_2 =="X" && position_3 = "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 =="O" && position_3 = "O"
       return win_combination
     else
       false
     end
-  end
   end
 end
