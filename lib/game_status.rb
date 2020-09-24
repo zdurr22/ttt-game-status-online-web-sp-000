684eef77a0c985de7d1a7c5f93b0c6064c860c5f
Zachary Durr <zdurr22@gmail.com>
2020-09-24 17:12:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,8 +28,9 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 =="O" && position_3 = "O"
       return win_combination
-    else
-      false
     end
+  else
+    false
+  end
   end
 end
