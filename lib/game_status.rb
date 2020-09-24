cd84ed86b579c0d50dbb21972506f0441a4cd3c4
Zachary Durr <zdurr22@gmail.com>
2020-09-24 18:21:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,3 +36,11 @@ end
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
 end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
